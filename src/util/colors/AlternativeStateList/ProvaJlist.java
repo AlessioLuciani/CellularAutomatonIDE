b5da422a492e5827c58ae02ed69bf226d1ffb99f
MaveriK198 <provastonage@gmail.com>
2018-05-17 23:27:27 +0200

    aggiunta alternativa JList Stati

---
+++ src/util/colors/AlternativeStateList/ProvaJlist.java
@@ -0,0 +1,50 @@
+package util.colors.AlternativeStateList;
+
+import java.awt.Color;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.ListModel;
+
+import util.colors.ColoredRGBLabel;
+
+
+
+public class ProvaJlist {
+
+	public static void main(String[] args) {
+
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frame.setSize(300, 300);
+		
+		JLabel lbl = new ColoredRGBLabel(Color.ORANGE);
+		JLabel lbl1 = new ColoredRGBLabel(Color.RED);
+		JLabel lbl2 = new ColoredRGBLabel(Color.BLUE);
+		JLabel lbl3 = new ColoredRGBLabel(Color.GREEN);
+		
+		
+		
+		JList<JLabel> labelList = new JList<>();
+		ListModel<JLabel> listModel = new DefaultListModel<>();
+		labelList.setModel(listModel);
+		labelList.setFixedCellHeight(30);
+		labelList.setCellRenderer(new JLabelJListRender());
+		labelList.setVisible(true);
+		labelList.setSize(100,100);
+		
+		
+		DefaultListModel<JLabel> model = (DefaultListModel<JLabel>)labelList.getModel();
+		model.addElement( lbl);
+		model.addElement( lbl1);
+		model.addElement( lbl2);
+		model.addElement( lbl3);
+		
+		frame.add(labelList);
+		frame.setVisible(true);
+
+	}
+
+}
