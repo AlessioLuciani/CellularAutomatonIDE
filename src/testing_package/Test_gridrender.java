12acb301f91ab17f7d8a8be756bb22cb84204176
asdfdsas <mirko.giacchini98@gmail.com>
2018-05-18 20:01:12 +0200

    bho, c'era un errore

--- src/testing_package/Test_gridrender.java
+++ src/testing_package/Test_gridrender.java
@@ -5,7 +5,6 @@ import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
-import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
